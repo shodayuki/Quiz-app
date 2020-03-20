adfcdfe799a4f5a7518d65a42c6be7afd3efe599
shodayuki <yshoda1227@gmail.com>
2020-03-20 11:35:34 +0900

    クラス実装

--- __tests__/src/models/Quiz.spec.js
+++ __tests__/src/models/Quiz.spec.js
@@ -29,4 +29,55 @@ describe('Quizクラスのテスト', () => {
       });
     });
   });
+
+  describe('getter', () => {
+    it('questionとcorrectAnswerのgetterが増える', () => {
+      const quizData = createMockQuiz();
+      const quiz = new Quiz(quizData);
+
+      expect( quiz.question ).toStrictEqual(quizData.question);
+      expect( quiz.correctAnswer ).toStrictEqual(quizData.correctAnswer);
+      expect( quiz.incorrectAnswers ).toStrictEqual(undefined);
+    });
+  });
+
+  describe('shuffleメソッド', () => {
+    it('シャッフルされる', () => {
+      const quizData = createMockQuiz();
+      const quiz = new Quiz(quizData);
+
+      const shuffledAnswers1 = quiz.shuffleAnswers();
+      const shuffledAnswers2 = quiz.shuffleAnswers();
+      expect(shuffledAnswers1).not.toStrictEqual(shuffledAnswers2);
+    });
+  });
+
+  describe('judgeCorrectAnswerメソッド', () => {
+    it('引数の値が正解ならtrue, 不正解ならfalseが通る', () => {
+      const quizData = createMockQuiz();
+      const quiz = new Quiz(quizData);
+
+      expect(quiz.judgeCorrectAnswer(quizData.correctAnswer))
+        .toStrictEqual(true);
+
+      quizData.incorrectAnswers.forEach(incorrectAnswer => {
+        expect(quiz.judgeCorrectAnswer(incorrectAnswer))
+          .toStrictEqual(false);
+      });
+    });
+  });
+
+  describe('クラスメソッド', () => {
+    describe('fetchAndCreateQuizzesメソッド', () => {
+      it('10件のQuizインスタンスが返る', async () => {
+        const quizzes = await Quiz.fetchAndCreateQuizzes();
+
+        expect ( Array.isArray(quizzes) ).toStrictEqual(true);
+        expect (quizzes.length).toStrictEqual(10);
+        quizzes.forEach(quiz => {
+          expect( quiz instanceof Quiz ).toStrictEqual(true);
+        });
+      });
+    });
+  });
 });
