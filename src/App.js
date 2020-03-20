31f87895fc3a1ea75f8872b9f9333afee5eba9f2
shodayuki <yshoda1227@gmail.com>
2020-03-20 12:00:29 +0900

    npm startでエラー

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Route
+} from 'react-router-dom';
+import Home from './components/Home/Home';
+import Quiz from './components/Quiz/Quiz';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/quiz" exact component={Quiz} />
+    </Router>
   );
 }
 
---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Quiz = () => {
+  return (
+    <h1>Quizです</h1>
+  );
+};
+
+export default Quiz;
