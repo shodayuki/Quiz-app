7c5ac515ad33fc345ba2030fe3565c867af8c43d
shodayuki <yshoda1227@gmail.com>
2020-03-20 14:44:28 +0900

    クイズ表示実装

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,9 +1,108 @@
 import React from 'react';
+import QuizModel from '../../models/Quiz';
+import Button from '../Button/Button';
 
-const Quiz = () => {
-  return (
-    <h1>Quizです</h1>
-  );
-};
+class Quiz extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      quizzes: [],
+      currentIndex : 0,
+      numberOfCorrects : 0
+    };
+  }
+
+  async componentDidMount() {
+    await this.restart();
+  }
+
+  async restart() {
+    this.setState({
+      quizzes: [],
+      currentIndex : 0,
+      numberOfCorrects : 0
+    });
+
+    const quizzes = await QuizModel.fetchAndCreateQuizzes();
+
+    this.setState({ quizzes });
+  }
+
+  selectAnswer(quiz, answer) {
+    let { currentIndex, numberOfCorrects } = this.state;
+    const isCorrect = quiz.judgeCorrectAnswer(answer);
+
+    if (isCorrect) {
+      numberOfCorrects++;
+      alert('Correct answer!!');
+    } else {
+      alert('Wrong answer... (The correct answer is "${quiz.correctAnswer}")');
+    }
+    currentIndex++;
+
+    this.setState({
+      currentIndex,
+      numberOfCorrects
+    });
+  }
+
+  render() {
+    const { quizzes, currentIndex } = this.state;
+
+    // 読み込み中
+    if (quizzes.length === 0) {
+      return this.renderLoading();
+    }
+
+    // クイズ中
+    if (quizzes.length > 0 && currentIndex < quizzes.length) {
+      return this.renderQuiz();
+    }
+
+    return (
+      <h1>Quizです</h1>
+    );
+  }
+
+  renderLoading() {
+    return (
+      <div>
+        <h1>クイズページ</h1>
+        <p>Now Loading...</p>
+        <hr/>
+        {/* <Link to="/">トップページへ</Link> */}
+      </div>
+    );
+  }
+
+  renderQuiz() {
+    const { currentIndex, quizzes } = this.state;
+
+    const quiz = quizzes[currentIndex];
+    const answers = quiz.shuffleAnswers().map((answer, index) => {
+      return (
+        <li key={index}>
+          <Button
+            onClickHandler={() => { this.selectAnswer(quiz, answer) }}
+          >
+            {answer}
+          </Button>
+        </li>
+      );
+    });
+
+    return (
+      <div>
+        <h1>クイズページ</h1>
+        <div>
+          <p>{quiz.question}</p>
+          <ul className="QuizList">{answers}</ul>
+        </div>
+        {/* <Link to="/">トップページへ</Link> */}
+      </div>
+    );
+  }
+}
 
 export default Quiz;
